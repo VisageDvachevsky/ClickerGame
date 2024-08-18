0f86c334b067b9be303d4bff61ef7ee8f635d927
Visage <mr.echxo@gmail.com>
2024-08-19 00:38:14 +0300

    deploy bat

---
+++ deploy.bat
@@ -0,0 +1,24 @@
+@echo off
+
+
+cd frontend\clickerwebapp
+
+call npm run build
+
+robocopy build ..\..\backend\public /E
+
+cd ..\..
+
+cd backend
+
+
+taskkill /F /IM node.exe
+
+timeout /t 2
+
+start cmd /k "node server.js"
+
+cd ..
+
+echo Deployment complete and server started!
+pause
